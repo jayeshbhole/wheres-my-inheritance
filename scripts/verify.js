4871b2bb6f2da30cebcdf18e78ca381768baf428
Jayesh Bhole <54071350+jayeshbhole@users.noreply.github.com>
2022-06-25 23:59:16 +0530

    Update verify.js

--- scripts/verify.js
+++ scripts/verify.js
@@ -1,10 +1,14 @@
 const hre = require("hardhat");
-
+const { dec } = require("../utils/helpers");
 try {
   hre
     .run("verify:verify", {
-      address: "0x",
-      constructorArguments: [""],
+      address: "0xF3852a2720Bf97277Ae716d9c19e3050357E9508",
+      contract: "/contracts/BasicSwitch.sol:BasicSwitch",
+      constructorArguments: [
+        dec(1, "ether"),
+        "0x3C5F48fAee7f50603071de0DaE955EAFb5293c46",
+      ],
     })
     .then(() => {
       console.log("Contract verified successfully");
